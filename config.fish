84a6c4b955dd851cb5f4269f95bb7232ae0d72bf
Elvis Pinares <epinaresg@mesa247.pe>
2024-06-24 20:44:11 -0500

    Ajustes

--- config.fish
+++ config.fish
@@ -1,7 +1,7 @@
-alias activate 'source venv/bin/activate.fish'
-alias makemigrations 'python manage.py makemigrations'
-alias migrate 'python manage.py migrate'
-alias runserver 'python manage.py runserver'
-alias test 'python manage.py test'
+alias p-activate 'source venv/bin/activate.fish'
+alias p-makemigrations 'python manage.py makemigrations'
+alias p-migrate 'python manage.py migrate'
+alias p-runserver 'python manage.py runserver'
+alias p-test 'python manage.py test'
 
 set -g theme_short_path yes
